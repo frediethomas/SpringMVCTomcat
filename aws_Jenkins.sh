5f41987f2572185a89bb88f13fb47e1d7f52ebdd
frediethomas <frediethomas@gmail.com>
2020-09-15 11:09:09 +0000

    minimal setups with jenkins

---
+++ aws_Jenkins.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+sudo yum -y update
+
+echo "Install Java JDK 8"
+sudo yum remove -y java
+sudo yum install -y java-1.8.0-openjdk-devel
+
+echo "Install git"
+sudo yum install -y git
+
+echo "Install  ekctl"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+echo "Install  kubectl"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo "Install  helm"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm repo update
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+	
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+sudo yum install jenkins -y
+
+
+wget http://mirrors.gigenet.com/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+
+sudo tar -zxvf apache-maven-3.6.3-bin.tar.gz -C /opt/
+
